57c54992b9d589a9becab5119c85758d35051739
Gahandi Sebastian Cervantes Hinojosa <gsch.sehi@gmail.com>
2025-11-19 01:06:30 -0600

    FIX - SonarCloud

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,5 @@ router.post('/refresh', refreshToken);
 router.post('/verificar-codigo', verificarCodigoActivacion);
 router.post('/reenviar-codigo', reenviarCodigoActivacion);
 router.post('/access-code', confirmarAcceso);
-
+//TEST SONAR CLOUD
 module.exports = router;
